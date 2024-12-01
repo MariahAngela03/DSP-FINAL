2d651c0e123f02fa69493b7ecadf03fde43b0b86
MariahAngela03 <152053075+MariahAngela03@users.noreply.github.com>
2024-12-01 15:04:24 +0800

    Update yolo_live.py

--- yolo_live.py
+++ yolo_live.py
@@ -1,4 +1,11 @@
 import cv2
+import time
+import streamlit as st
+from ultralytics import YOLO
+from draw_utils_live import plot_boxes_live, color_map_live
+
+# Function to handle live webcam detection
+def live_detectionimport
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -17,3 +24,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
